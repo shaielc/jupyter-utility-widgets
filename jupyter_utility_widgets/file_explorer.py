26de0ac79c267254ef091e9c1ba3cd437cd96a34
Shy-El Cohen <shaielshmoel@gmail.com>
2023-05-09 11:00:25 +0300

    file explorer

--- jupyter_utility_widgets/file_explorer.py
+++ jupyter_utility_widgets/file_explorer.py
@@ -1,4 +1,4 @@
-from ipywidgets import Text, HBox, Valid, link, Dropdown
+from ipywidgets import Text, HBox, Valid, link, Dropdown, SelectMultiple
 from traitlets import Unicode, observe
 import os
 
@@ -54,48 +54,15 @@ class DirectoryChooser(HBox):
             path_solver = os.path.realpath
         self.directory = path_solver(os.path.join(self.directory, change.new))
 
-# class DirectoryChooser:
-#     path_inp = Text()
-#     select_subdir = Dropdown()
-#     select_valid_subdir = Dropdown()
-#     set_dir = Button(description="Set dir")
-#     directory = directory
-#     save_directory = save_directory
+class FileChooser(HBox):
+    directory = Unicode()
+    def __init__(self, *args, **kwargs) -> None:
+        self.select = SelectMultiple()
+        super().__init__([self.select], *args, **kwargs)
     
-#     @classmethod
-#     def set_directory(cls, directory=None):
-#         if directory is None:
-#             directory = cls.path_inp.value
-#         if os.path.isdir(directory):
-#             cls.directory = directory
-#             cls.path_inp.value = directory
-#             subdirs = sorted([e for e in os.listdir(cls.directory) if os.path.isdir(os.path.join(cls.directory, e) )])
-#             cls.select_subdir.options = [""] + subdirs + [".."]
-#             valid_subdirs = [e for e in subdirs if is_formatted_dir(os.path.join(cls.directory, e))] 
-#             cls.open_log_tree.disabled = len(valid_subdirs) == 0
-#             cls.select_valid_subdir.options = valid_subdirs
-            
-            
-#     @classmethod
-#     def open_burst_dir(cls, directory):
-#         CurrentBurst.init(directory)
+    @observe("directory")
+    def _new_directory(self, change):
+        path = change.new
         
-#     @classmethod
-#     def display(cls):
-#         display(HBox([cls.path_inp, cls.set_dir, cls.select_subdir,cls.open_burst_dir_btn,]),HBox([cls.select_valid_subdir ,cls.open_log_tree]) )
-        
-#     @classmethod
-#     def update_subdir(cls, evt):
-#         cls.select_subdir.unobserve(cls.update_subdir, names=["value"])
-#         cls.set_directory(os.path.relpath(os.path.join(cls.directory, cls.select_subdir.value)))
-#         cls.select_subdir.observe(cls.update_subdir, names=["value"])
-        
-#     @classmethod
-#     def init(cls):
-#         cls.path_inp.value = cls.directory
-#         cls.set_directory()
-#         cls.set_dir.on_click(lambda btn: cls.set_directory())
-#         cls.select_subdir.observe(cls.update_subdir, names=["value"])
-#         cls.open_burst_dir_btn.on_click(lambda btn: cls.open_burst_dir(cls.directory))
-#         cls.open_log_tree.on_click(lambda btn: cls.open_burst_dir(os.path.join(cls.directory, cls.select_valid_subdir.value, "detection","raw")))
-        
+        if os.path.isdir(path):
+            self.select.options = list(filter(lambda path: os.path.isfile(os.path.join(self.directory,path)),[".."] +os.listdir(path)))
