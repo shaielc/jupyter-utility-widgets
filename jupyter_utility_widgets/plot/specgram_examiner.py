f10d33f6e3d81869c0364c16d472f8f22ea756b0
Shy-El Cohen <shaielshmoel@gmail.com>
2023-06-15 11:10:10 +0300

    removed "wraps" experiments

--- jupyter_utility_widgets/plot/specgram_examiner.py
+++ jupyter_utility_widgets/plot/specgram_examiner.py
@@ -2,7 +2,7 @@ from jupyter_utility_widgets.plot.figures.specgram import SpecgramPlot
 from matplotlib.widgets import SpanSelector
 from ipywidgets import HBox
 from matplotlib import pyplot as plt
-from functools import wraps, update_wrapper
+from functools import wraps
 
 class SpectrogramExaminer(HBox):
     def __init__(self, keep_selection=False, **kwargs):
@@ -35,18 +35,14 @@ class SpectrogramExaminer(HBox):
 
         super().__init__(children=[self.full_spec, self.zoom_spec], **kwargs)
         self.data = None
-        
-        self.set_params = update_wrapper(lambda *args, **kwargs: self._set_params, plt.specgram)
-
-    def test(self, *args):
-        print(self, *args)
 
     def on_select(self, tmin, tmax):
         self.last_span = (tmin, tmax)
         xmin, xmax = int(tmin * self.full_spec.sample_rate), int(tmax * self.full_spec.sample_rate)
         self.zoom_spec.update(self.data[max(0,xmin):min(len(self.data),xmax)])
-
-    def _set_params(self, *args, **kwargs):
+    
+    @wraps(plt.specgram)
+    def set_params(self, *args, **kwargs):
         self.full_spec.set_params(*args, **kwargs)
         self.zoom_spec.set_params(*args, **kwargs)
 
