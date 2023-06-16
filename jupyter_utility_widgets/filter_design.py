0b38bbb2a2918f0378ea36d4578b72d394b1ecef
Shy-El Cohen <shaielshmoel@gmail.com>
2023-06-16 11:45:13 +0300

    filter design widget

--- jupyter_utility_widgets/filter_design.py
+++ jupyter_utility_widgets/filter_design.py
@@ -1,45 +1,72 @@
 from jupyter_utility_widgets.plot.filter import Filter
+from jupyter_utility_widgets.utility.numpy_parameter_converter import NumpyFunctionUIFactory
 from ipywidgets import HBox, VBox, Dropdown, BoundedIntText, BoundedFloatText
-from traitlets import Any
+from traitlets import Any, Unicode
 from scipy.signal import windows, firwin
 available_windows = list(filter(lambda s: not (s.startswith("_") or s in ["get_window"]), dir(windows))) + ["manual"]
-class WindowSelect(Dropdown):
+class WindowSelect(VBox):
     manual = Any()
+    value = Any()
     
     def __init__(self, **kwargs):
-        super().__init__(options=available_windows, **kwargs)
+        self.window_func_selector = Dropdown(options=available_windows, **kwargs)
+        self.extras = VBox([])
+
+        super().__init__(children=[self.window_func_selector, self.extras])
+
+        self.window_func_selector.observe(lambda evt: self._handle_window_change(evt.new), names=["value"])
+
+    def _handle_window_change(self, window_name):
+        self.set_extras(window_name)
+        self.update_value()
+    
+    def set_extras(self, window_name):
+        func = getattr(windows, window_name)
+        parameters, parameters_types = NumpyFunctionUIFactory.get_parameter_types(func)
+        parameters_types.pop('M')
+        parameters_types.pop('sym')
+
+        for child in self.extras.children:
+            child.unobserve_all()
+
+        self.extras.children = NumpyFunctionUIFactory.construct_widgets(parameters, parameters_types)
+        
+        for child in self.extras.children:
+            child.observe(lambda evt: self.update_value(), names=["value"])
+
+    def update_value(self,):
+        self.value = self.window
     
     @property
     def window(self,):
-        if self.value == "manual":
+        window_name = self.window_func_selector.value
+        if  window_name == "manual":
             return self.manual
-        return getattr(windows, self.value)
+        return (window_name,) + tuple(child.value for child in self.extras.children)
 
 class FilterDesignControls(VBox):
     value = Any()
     def __init__(self, **kwargs):
         self.window_selector = WindowSelect(description="Win")
         self.window_length = BoundedIntText(min=0, value=64, max=2**20, description="N")
-        self.cutoff_input = BoundedFloatText(value=0.25, min=1e-12, max=0.5, description="w0")
-        self.width_input = BoundedFloatText(value=0.001, min=1e-12, max=0.5, description="w1")
+        self.cutoff_input = BoundedFloatText(value=0.25, min=1e-12, max=1, description="w0")
         
         children= [
-            self.window_selector,
             self.window_length, 
             self.cutoff_input,
-            self.width_input
+            self.window_selector,
         ]
         super().__init__(children, **kwargs)
-        
+
         for child in children:
             child.observe(lambda evt: self.calc_filt(),names=["value"])
     
     def calc_filt(self, ):
+        print(self.window_selector.value)
         self.value = firwin(
             self.window_length.value,
             self.cutoff_input.value,
-            self.width_input.value,
-            self.window_selector.window
+            window=self.window_selector.value
         )
 
 class FilterDesign(HBox):
---
+++ jupyter_utility_widgets/utility/numpy_parameter_converter.py
@@ -0,0 +1,56 @@
+from typing import List, Literal, Callable
+from ipywidgets import IntText, FloatText, Checkbox, Box
+from numpydoc.docscrape import NumpyDocString, Parameter
+from warnings import warn
+
+"""
+NOTE: This is an exremely early prototype of the feature and is missing A LOT of possible options
+e.g.:
+* Types
+* Parsing of the "type" section
+* Automation of func->widget
+etc
+"""
+
+known_types = {
+    "int": int,
+    "float": float,
+    "bool": bool
+}
+
+type_text_widget = {
+    int: IntText,
+    float: FloatText,
+    bool: Checkbox
+}
+
+SKIP_PARAMETER = 0
+
+class NumpyFunctionUIFactory:
+    @staticmethod
+    def get_parameter_types(func: Callable):
+        parsed = NumpyDocString(func.__doc__)
+        parameters: List[Parameter] = parsed["Parameters"]
+        return parameters, {parameter.name: known_types.get(parameter.type) for parameter in parameters}
+
+    @staticmethod
+    def construct_widgets(parameters: List[Parameter], parameter_types=None):
+        if parameter_types is None:
+            parameter_types = NumpyFunctionUIFactory.get_parameter_types(parameters)
+
+        widgets = []
+        for parameter in parameters:
+            current_type = parameter_types.get(parameter.name, SKIP_PARAMETER)
+            
+            if current_type is None:
+                if "optional" not in parameter.type:
+                    warn("Missing type '%s' for non-optional parameter %s" % (parameter.type, parameter.name))
+                continue
+            
+            if current_type == SKIP_PARAMETER:
+                continue
+
+            widgets.append(type_text_widget[current_type](description=parameter.name))
+        
+        return widgets
+            
