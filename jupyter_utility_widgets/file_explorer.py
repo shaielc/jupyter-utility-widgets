e634951636ed8435bbaac16cff070393c18dc7be
Shy-El Cohen <shaielshmoel@gmail.com>
2023-09-12 15:59:52 +0300

    file explorer QOL, sort, filter ext and composite

--- jupyter_utility_widgets/file_explorer.py
+++ jupyter_utility_widgets/file_explorer.py
@@ -1,6 +1,24 @@
-from ipywidgets import Text, HBox, Valid, link, Dropdown, SelectMultiple
-from traitlets import Unicode, observe, Bool
+from ipywidgets import Text, HBox, Valid, link, Dropdown, SelectMultiple, Select, VBox, ValueWidget, Button
+from traitlets import Unicode, dlink, observe, Bool
 import os
+
+def get_entries(directory, path, allow_files=True):
+    check = os.path.exists
+    if not allow_files:
+        check = os.path.isdir
+
+    return list(sorted(filter(lambda path: check(os.path.join(directory,path)),[".."] +os.listdir(path))))
+
+def create_filter_ext(extensios, directory, allow_dir=True):
+    def ext_filter(filename):
+        path = os.path.join(directory, filename)
+        if allow_dir and os.path.isdir(path):
+            return True
+        if not os.path.isfile(path):
+            return False
+        _, ext = os.path.splitext(path)
+        return ext in extensios
+    return ext_filter
 
 class PathInput(HBox):
     value = Unicode()
@@ -39,7 +57,7 @@ class DirectoryChooser(Dropdown):
 
         if os.path.isdir(path):
             self.relative_mode = path[0] != "/"
-            self.options = list(filter(lambda path: os.path.isdir(os.path.join(self.directory,path)),[".."] +os.listdir(path)))
+            self.options = get_entries(self.directory, path, allow_files=False)
     
     @observe("value")
     def _update_path(self, change):
@@ -51,13 +69,56 @@ class DirectoryChooser(Dropdown):
             path_solver = os.path.realpath
         self.directory = path_solver(os.path.join(self.directory, change.new))
 
-class FileChooser(SelectMultiple):
+class FileChooser(Select):
     directory = Unicode()
+
+    def __init__(self, *args, extensions=None, **kwargs):
+        self.extensions = extensions
+
+        super().__init__(*args, **kwargs)
 
     @observe("directory")
     def _new_directory(self, change):
         path = change.new
         
         if os.path.isdir(path):
-            self.options = list(filter(lambda path: os.path.isfile(os.path.join(self.directory,path)),[".."] +os.listdir(path)))
+            options = get_entries(self.directory, path)
+            if self.extensions is not None:
+                options = list(filter(create_filter_ext(self.extensions, self.directory), options))
+            self.options = options
+
+    @property
+    def path(self,):
+        return os.path.join(self.directory,self.value)
+
+
+class FileExplorer(VBox, ValueWidget):
+    value = Unicode()
+
+    def __init__(self, directory="", extensions=None, **kwargs):
+        self.directory_explorer = DirectoryChooser()
+        self.file_explorer = FileChooser(extensions=extensions)
+        self.input = PathInput()
+        self.select_button = Button(description="Select")
+        self.select_button.on_click(self.on_select)
+        
+        link((self.input, "value"), (self.directory_explorer, "directory"))
+        dlink((self.input, "value"), (self.file_explorer, "directory"))
+        
+        children = [
+            HBox([self.input, self.directory_explorer]),
+            HBox([self.file_explorer, self.select_button])
+        ]
+        super().__init__(children, **kwargs)
+        self.directory = directory
+
+    def on_select(self, evt):
+        self.value = self.file_explorer.path
+
+    @property
+    def directory(self,):
+        return self.directory_explorer.directory
     
+    @directory.setter
+    def directory(self, value):
+        self.directory_explorer.directory = value
